a4841d96b492282990bdec23993e1bca0656badd
Wellington <junior.wel02@exemplo.br>
2021-11-03 20:33:29 -0300

    Updates

---
+++ precache-manifest.c770f6d75e248aae648a9491eb8b11a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f9a5f14292adf4f9b79d97ba3e1bcd",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "3ffcf2258f090ff8dead",
+    "url": "/trybewallet/static/css/main.4f9077d3.chunk.css"
+  },
+  {
+    "revision": "17431899ffeb30a1e90c",
+    "url": "/trybewallet/static/js/2.bc366f15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.bc366f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ffcf2258f090ff8dead",
+    "url": "/trybewallet/static/js/main.622be65b.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
